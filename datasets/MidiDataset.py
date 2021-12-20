9e23fa6efa176df88bb0d9c54aa6cc0b91256407
AHMET ERCEM BULUT <e2171387@ceng.metu.edu.tr>
2021-12-20 12:59:18 +0100

    started on the data loader

---
+++ datasets/MidiDataset.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, division
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+
+class MidiDataset(Dataset):
+    """Midi Songs dataset."""
+
+    def __init__(self, root_folder='datasets/', sets=[]: list[str]):
+        """
+        Args:
+            sets (list[string]): names of the set of data to be used
+                                 if empty list all under dataset folder is used
+        """
+        self.root_dir = root_folder
+        self.sets = sets
+        if self.sets == []:
+            pass
+            
+            
+
+    def getListOfFiles(dirName): 
+        pass
